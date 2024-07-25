ef4c4f9d4afa3191b4aaf45c30d8563536431179
Theerapatxx <kottee.5870@gmail.com>
2024-07-25 14:29:20 +0700

    Create stringDemo.java

---
+++ stringDemo.java
@@ -0,0 +1,9 @@
+public class stringDemo {
+    public static void main(String[] args) {
+    String greeting ;
+    greeting = "Good morning";
+    String name = "computer";
+    System.err.println(greeting + name);
+    }
+    
+}
