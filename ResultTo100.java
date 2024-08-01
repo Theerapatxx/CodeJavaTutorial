89122d4a0bef90dcd52f7abcb1f383e581080444
Theerapatxx <kottee.5870@gmail.com>
2024-08-01 10:20:08 +0700

    Create ResultTo100.java

---
+++ ResultTo100.java
@@ -0,0 +1,13 @@
+public class ResultTo100 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int number = 1; 
+        while (number <= 100) {
+            sum += number; 
+            System.out.println(" "+number);
+            number++; 
+            
+        }
+        System.out.println("The sum of numbers from 1 to 100 is: " + sum);
+    }
+}
