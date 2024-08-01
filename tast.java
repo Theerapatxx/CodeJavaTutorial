03c47caa00938cc831d2e11e6dccc070a4d33f85
Theerapatxx <kottee.5870@gmail.com>
2024-08-01 09:22:34 +0700

    Create tast.java

---
+++ tast.java
@@ -0,0 +1,15 @@
+public class tast {
+    public static void main(String[] args) {
+        double x = 2.5;
+        double y = -1.5;
+        int n = 4;
+
+        // คำนวณค่า
+        double result = x + n * y - (x + n) * y;
+
+        // แสดงผลลัพธ์
+        System.out.println("Result: " + result);
+    }
+
+    
+}
