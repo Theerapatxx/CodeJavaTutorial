9b606bfb837c57bedeabb7453829a801d66b58d0
Theerapatxx <kottee.5870@gmail.com>
2024-08-02 12:50:04 +0700

    Create PrimeCh.java

---
+++ PrimeCh.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class PrimeCh {
+    public static void main(String[] args) {
+        int number;
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("What number would you like to tast ?");
+        number = input.nextInt();
+        if(isPrime(number)){
+            System.out.println(number+"is a prime number.");
+        }
+        else{
+            System.out.println(" is not prime.");
+        }
+    }
+    public static boolean isPrime(int p){
+        if(p <= 1){
+            return false;
+        }
+        else if(p == 2){
+            return true;
+        }
+            for(int i = 3; i < p; i += 2){
+                if(p % i == 0){
+                    return false;
+                }else{
+
+                }
+            }
+            return true;
+    }
+}
